f3ddd230880949012e1e153f5bb10cc2e58f9bf3
Herberth Kagiya <tayki.heberth@gmail.com>
2022-08-03 10:52:16 -0300

    App working

--- app/src/main/java/com/kagiya/randomnumbergenerator/MainActivity.kt
+++ app/src/main/java/com/kagiya/randomnumbergenerator/MainActivity.kt
@@ -2,10 +2,27 @@ package com.kagiya.randomnumbergenerator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
+
+private lateinit var randomNumberButton: Button;
+private lateinit var resultTextView: TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        randomNumberButton = findViewById(R.id.random_number_button)
+        resultTextView = findViewById(R.id.result_textView)
+
+        randomNumberButton.setOnClickListener{
+            val result = Random.nextInt(1, 10000);
+
+            val stringResult = resources.getString(R.string.random_number_result)
+
+            resultTextView.setText("$stringResult $result")
+        }
     }
 }
